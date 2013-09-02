09d7c9087663212c21247128e4975986af1179f5
Virgis <virgis@kitoks.lt>
2013-09-02 18:03:00 +0300

    Pakeitimai

---
+++ cluster/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Virginijus
+ *
+ * Created on September 2, 2013, 5:59 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
